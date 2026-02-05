ef5c886466b041d581b2b11a0da71a47a3befc63
Hpayeur <hailpaye017@gmail.com>
2026-02-05 15:03:32 -0700

    Try, If, Catch

--- 03-task-manager/starter/Controllers/tasks.js
+++ 03-task-manager/starter/Controllers/tasks.js
@@ -1,13 +1,33 @@
-const getAllTasks = (req, res) => {
-  res.send("Get all Task Items!");
+const Task = require("../Models/Tasks");
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const createTasks = (req, res) => {
-  res.json(req.body);
+const createTasks = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const getTasks = (req, res) => {
-  res.json({ id: req.params.id });
+const getTasks = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskId });
+    res.status(200).json({ id: req.params.id });
+    if (!task) {
+      res.status(404).json({ msg: `No task with ID: ${taskID}` });
+    }
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const updateTasks = (req, res) => {
