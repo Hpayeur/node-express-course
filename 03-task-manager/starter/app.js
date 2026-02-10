fb99e0db6f0fda2190f8257be68b65b8ea96786e
Hpayeur <hailpaye017@gmail.com>
2026-02-10 15:54:30 -0700

    Routes/tasks.js/app.js

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -4,10 +4,14 @@ const servers = dns.getServers();
 console.log("Node.js is using these DNS servers:", servers);
 
 const express = require("express");
+const tasks = require("./routes/tasks");
 const app = express();
 app.get("/hello", (req, res) => {
   res.send("Task Manager App");
 });
+
+app.use("/api/v1/tasks", tasks);
+
 const port = 3000;
 
 app.listen(port, console.log(`Server is listening on port ${port}...`));
