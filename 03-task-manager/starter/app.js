f3782ff0bfdb141efbf57995f06ea07ea2d3a6ff
Hpayeur <hailpaye017@gmail.com>
2026-02-10 15:46:57 -0700

    App.js Update

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,13 @@
-console.log('Task Manager App')
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+const servers = dns.getServers();
+console.log("Node.js is using these DNS servers:", servers);
+
+const express = require("express");
+const app = express();
+app.get("/hello", (req, res) => {
+  res.send("Task Manager App");
+});
+const port = 3000;
+
+app.listen(port, console.log(`Server is listening on port ${port}...`));
