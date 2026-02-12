af5d36090523d4d85c6deec7d1450eb546c32282
Hpayeur <hailpaye017@gmail.com>
2026-02-12 16:58:17 -0700

    controllers

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,45 +1,45 @@
-const dns = require("node:dns");
+const dns = require("node:dns/promises");
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 const servers = dns.getServers();
 console.log("Node.js is using these DNS servers:", servers);
-console.log("04 Store API");
-
 require("dotenv").config();
-
-//Async Errors
 require("express-async-errors");
+
 const express = require("express");
 const app = express();
+
 const connectDB = require("./db/connect");
 const productsRouter = require("./routes/products");
+
 const notFoundMiddleware = require("./middleware/not-found");
 const errorMiddleware = require("./middleware/error-handler");
 
-//MiddleWare
+// middleware
 app.use(express.json());
 
-//Routes
+// routes
+
 app.get("/", (req, res) => {
-  res.send('<h1> Store API </h1><a href="/api/v1/products">Products Route</a>');
+  res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
 });
 
 app.use("/api/v1/products", productsRouter);
 
-//Products Route
+// products route
+
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
 
 const port = process.env.PORT || 3000;
 
-//ConnectDB
 const start = async () => {
   try {
+    // connectDB
     await connectDB(process.env.MONGO_URI);
-    app.listen(port, console.log(`Server is listening to port ${port}...`));
+    app.listen(port, () => console.log(`Server is listening port ${port}...`));
   } catch (error) {
     console.log(error);
   }
 };
-start();
 
-// Course Video: https://www.youtube.com/watch?v=qwfE7fSVaZM&t=179s Time:
+start();
--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,13 +1,18 @@
+const Product = require("../models/product");
+
 const getAllProductsStatic = async (req, res) => {
-  throw new Error("Testing Async Errors");
-  res.status(200).json({ msg: "Products Testing This Route" });
+  const products = await Product.find({ featured: true });
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "Testing Products Route" });
+  const products = await Product.find(req.query);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
   getAllProducts,
   getAllProductsStatic,
 };
+
+//Youtube Course: https://www.youtube.com/watch?v=qwfE7fSVaZM&t=179s Timestamp:  4:00:00
