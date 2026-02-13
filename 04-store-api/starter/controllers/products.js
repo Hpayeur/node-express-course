214bbe0d0946eda75f6ff53ac3ff95b95517f364
Hpayeur <hailpaye017@gmail.com>
2026-02-12 17:33:38 -0700

    projects.js

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,12 +1,27 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({ featured: true });
+  const search = "a";
+  const products = await Product.find({
+    name: { $regex: search, $options: "i" },
+  });
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const products = await Product.find(req.query);
+  const { featured, company, name } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+  console.log(queryObject);
+  const products = await Product.find(queryObject);
   res.status(200).json({ products, nbHits: products.length });
 };
 
@@ -15,4 +30,4 @@ module.exports = {
   getAllProductsStatic,
 };
 
-//Youtube Course: https://www.youtube.com/watch?v=qwfE7fSVaZM&t=179s Timestamp:  4:00:00
+//Youtube Course: https://www.youtube.com/watch?v=qwfE7fSVaZM&t=179s Timestamp:  4:10:00
