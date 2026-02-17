dc9aea712a72ffa0462e477b3eda015528139c65
Hpayeur <hailpaye017@gmail.com>
2026-02-17 13:26:39 -0700

    Controllers Products.js

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,15 +1,16 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({})
-    .sort("-name price")
+  const products = await Product.find({ price: { $gt: 30 } })
+    .sort("price")
     .select("name price");
+
   res.status(200).json({ products, nbHits: products.length });
 };
-
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
   const queryObject = {};
+
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
   }
@@ -19,9 +20,29 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: "i" };
   }
-  console.log(queryObject);
-  let result = await Product.find(queryObject);
+  if (numericFilters) {
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eq",
+      "<": "$lt",
+      "<=": "$lte",
+    };
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`,
+    );
+    const options = ["price", "rating"];
+    filters = filters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-");
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
+  }
 
+  let result = Product.find(queryObject);
   if (sort) {
     const sortList = sort.split(",").join(" ");
     result = result.sort(sortList);
@@ -29,8 +50,17 @@ const getAllProducts = async (req, res) => {
     result = result.sort("createdAt");
   }
 
-  console.log(queryObject);
-  let products = await Product.find(queryObject);
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
+  const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
 
@@ -39,4 +69,4 @@ module.exports = {
   getAllProductsStatic,
 };
 
-//Youtube Course: https://www.youtube.com/watch?v=qwfE7fSVaZM&t=179s Timestamp:  4:31:37
+//Youtube Course: https://www.youtube.com/watch?v=qwfE7fSVaZM&t=179s Timestamp:
